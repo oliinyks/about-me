c7f8bf5f038016c1a8c8f94efbdd9fdf9862e4d2
oliinyks <101010916+oliinyks@users.noreply.github.com>
2022-09-02 14:50:57 +0000

    Deploying to gh-pages from @ oliinyks/about-me@8743d46dca948807d5fcc1adb66789fd51043024 🚀

---
+++ index.49b503cf.js
@@ -0,0 +1,2 @@
+(()=>{const e=document.querySelector("#toggle"),t=document.querySelector(".mobile-menu__list");e.addEventListener("click",(function(){t.classList.toggle("is-open")}))})();
+//# sourceMappingURL=index.49b503cf.js.map
---
+++ index.ddec5897.js
@@ -0,0 +1,2 @@
+!function(){var e,t;e=document.querySelector("#toggle"),t=document.querySelector(".mobile-menu__list"),e.addEventListener("click",(function(){t.classList.toggle("is-open")}))}();
+//# sourceMappingURL=index.ddec5897.js.map

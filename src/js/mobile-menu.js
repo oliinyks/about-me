62087db9a8668876bb07412c525934dc4397fbc5
Olijnyks <101010916+oliinyks@users.noreply.github.com>
2022-09-04 14:29:37 +0300

    add portfolio, footer

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,10 +1,23 @@
 (() => {
   const checboxEl = document.querySelector('#toggle');
-  const mobileMenuRef = document.querySelector('.mobile-menu__list');
+  const mobileMenuEl = document.querySelector('.mobile-menu__list');
+  const toggleEl = document.querySelector('#toggle');
 
   checboxEl.addEventListener('click', onChecboxClick);
+  mobileMenuEl.addEventListener('click', removeMenu);
 
   function onChecboxClick() {
-    mobileMenuRef.classList.toggle('is-open');
+    mobileMenuEl.classList.toggle('is-open');
+  }
+
+  function removeMenu(event) {
+    if (event.target.classList.contains('mobile-menu__link')) {
+      toggleEl.checked = false;
+      return onChecboxClick();
+    }
+    if (!event.target.classList.contains('mobile-menu__items')) {
+      toggleEl.checked = false;
+      return onChecboxClick();
+    }
   }
 })();

f53b8b49ed3cb9d5ceb28a63627cadc7a3012bb6
Olijnyks <101010916+oliinyks@users.noreply.github.com>
2022-09-27 11:37:12 +0300

    add team project

--- src/js/arrow-up and wave.js
+++ src/js/arrow-up and wave.js
@@ -1,26 +1,21 @@
 const goToTopEl = document.getElementById('goToTop');
 const waveEl = document.querySelector('.wave-box');
-window.onscroll = function () {
-  scrollFunction();
-};
+const header = document.querySelector('.header');
 
-function scrollFunction() {
-  goToTopEl.style.transition = 'var(--bounce) 0.1s';
-  waveEl.style.transition = 'var(--bounce) 0.1s';
-  if (
-    document.body.scrollTop > 150 ||
-    document.documentElement.scrollTop > 150
-  ) {
-    goToTopEl.style.opacity = '1';
-    waveEl.style.opacity = '0';
+function arrowScrollTop() {
+  if (window.scrollY > 50) {
+	goToTopEl.style.opacity = '1';
+	    waveEl.style.opacity = '0';
   } else {
-    goToTopEl.style.opacity = '0';
+	goToTopEl.style.opacity = '0';
     waveEl.style.opacity = '1';
   }
-}
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE     and Opera
+  goToTopEl.addEventListener('click', () => {
+	header.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  });
 }
 
-goToTopEl.addEventListener('click', topFunction);
+window.addEventListener('scroll', arrowScrollTop);
